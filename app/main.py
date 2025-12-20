9ae37b54ee913e282a3d06405ee0b7c84fd1e248
Chouwzi <chouwzi@proton.me>
2025-12-20 11:55:27 +0700

    fix: remove app arg in lifespan func

--- app/main.py
+++ app/main.py
@@ -5,6 +5,6 @@ from app.db.database import engine
 app = FastAPI(title="PeerHost", version="0.1.0")
 
 app.on_event("startup")
-def lifespan(app: FastAPI):
+def lifespan():
   # Tạo bảng
   SQLModel.metadata.create_all(engine)

ae689ab986f3b77d1242bc088914225f00dd7d93
Chouwzi <chouwzi@proton.me>
2025-12-20 11:54:30 +0700

    fix: remove context manager

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from sqlmodel import SQLModel
 from app.db.database import engine
-from contextlib import contextmanager
 
-@contextmanager
+app = FastAPI(title="PeerHost", version="0.1.0")
+
+app.on_event("startup")
 def lifespan(app: FastAPI):
   # Tạo bảng
   SQLModel.metadata.create_all(engine)
-  
-app = FastAPI(title="PeerHost", version="0.1.0", lifespan=lifespan)
